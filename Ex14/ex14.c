ec8ac3519d5c305b4f0316a71a11c5700c1f9041
Daniel Petersen <kveratis@gmail.com>
2013-04-02 15:25:07 -0600

    Refactor Ex14.c to use less functions and use string function to deterine length rather than null

--- Ex14/ex14.c
+++ Ex14/ex14.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declarations
 void print_arguments(int argc, char* argv[]);
 void print_letters(char arg[]);
-int can_print_it(char ch);
 
 int main(int argc, char* argv[])
 {
@@ -23,20 +23,16 @@ void print_arguments(int argc, char* argv[])
 
 void print_letters(char arg[])
 {
-	for(int i = 0; arg[i] != '\0'; i++)
+	int length = strlen(arg);
+	for(int i = 0; i < length; i++)
 	{
 		char ch = arg[i];
 
-		if(can_print_it(ch))
+		if(isalpha(ch) || isblank(ch))
 		{
 			printf("'%c' == %d ", ch, ch);
 		}
 	}
 
 	printf("\n");
-}
-
-int can_print_it(char ch)
-{
-	return isalpha(ch) || isblank(ch);
 }
